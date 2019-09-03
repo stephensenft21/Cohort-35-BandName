703e05e580d66c0099cca8e607b531993bd810b3
Stephen Senft <stephensenft21@gmail.com>
2019-09-03 15:29:13 -0500

    finished project

---
+++ src/js/main.js
@@ -0,0 +1,13 @@
+let bandNumber = 1
+
+const takeNumber = function (bandName) {
+    console.log(`${bandNumber}. ${bandName}`);
+   return bandNumber++;
+  
+}
+
+const scum = takeNumber("Galactic Scum")
+console.log(scum)  // This should print "1. Galactic Scum" in the console
+
+const under = takeNumber("Underdogs")
+console.log(under)  // This should print "2. Underdogs" in the console
